4ab5cde207f5def0e0f53615fd94bbea3b9cab3a
capaci <rafaelcapacipereira@gmail.com>
2018-02-12 01:40:38 -0200

    add a simple proof of work mechanism

--- src/main.js
+++ src/main.js
@@ -3,22 +3,34 @@ const sha256 = require('crypto-js/sha256')
 
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
-        this.index = index
+        this.index = index 
         this.timestamp = timestamp
         this.data = data
         this.previousHash = previousHash
         this.hash = this.calculateHash()
+        this.nonce = 0
     }
 
     calculateHash() {
         let dataString = JSON.stringify(this.data)
-        return sha256(`${this.index}${this.previousHash}${this.timestamp}${dataString}`).toString()
+        return sha256(`${this.index}${this.previousHash}${this.timestamp}${dataString}${this.nonce}`).toString()
+    }
+
+    mine(difficulty) {
+        // calculate hash until the first difficulty characters are '0'
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++
+            this.hash = this.calculateHash()
+        }
+
+        console.log(`\u26cf Block #${this.index} mined: ${this.hash}`)
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()]
+        this.difficulty = 2
     }
 
     createGenesisBlock() {
@@ -32,6 +44,7 @@ class Blockchain {
     addBlock({ data }) {
         let previousHash = this.getLatestBlock().hash
         let newBlock = new Block(this.chain.length, moment().unix(), data, previousHash)
+        newBlock.mine(this.difficulty)
         this.chain.push(newBlock)
     }
 
@@ -54,8 +67,10 @@ class Blockchain {
 }
 
 let blockchain = new Blockchain()
+console.log('Mining block #1...')
 blockchain.addBlock({ data: 'First block' })
+
+console.log('Mining block #2...')
 blockchain.addBlock({ data: 'Second block' })
+console.log('Mining block #3...')
 blockchain.addBlock({ data: 'Third block' })
-console.log(JSON.stringify(blockchain, null, 2))
-console.log('Is valid blockchain => ', blockchain.isChainValid())
